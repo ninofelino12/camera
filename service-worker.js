ef317a248ba23b99ac46b0572a07fe94e60ba634
ninofelino12 <ninofelino12@gmail.com>
2026-03-24 13:13:10 +0700

    2

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'kamera-app-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null)
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
